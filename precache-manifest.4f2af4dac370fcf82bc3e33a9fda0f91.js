0d0fb76c562d6604015fd4d4712d7b608a7fb1f4
harishara98 <harishmarabathula@gmail.com>
2020-01-24 05:04:04 -0800

    Updates

---
+++ precache-manifest.4f2af4dac370fcf82bc3e33a9fda0f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4f9fb1228d669d83a22386ff6041df",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a74cd326c4c02d89b42",
+    "url": "/monsters-rolodex/static/css/main.67e5b0af.chunk.css"
+  },
+  {
+    "revision": "6eee4035cea7dc8cba15",
+    "url": "/monsters-rolodex/static/js/2.0c532c32.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.0c532c32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a74cd326c4c02d89b42",
+    "url": "/monsters-rolodex/static/js/main.21d11766.chunk.js"
+  },
+  {
+    "revision": "202a145f686da1399a48",
+    "url": "/monsters-rolodex/static/js/runtime-main.9f2e98a0.js"
+  }
+]);
